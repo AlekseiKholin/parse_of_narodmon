c9f24b4ede3a0d5da6da4d0aa2952bb6d164406b
AlekseiKholin <alex_athlete1991@mail.ru>
2019-04-14 11:22:47 +0700

    изменение README

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # parse_of_narodmon
 
-Здесь будет описание 
+Скрипт по получению значения температуры с указанного датчтка
+
+Запрос на сервер:
+resp = requests.get('http://narodmon.ru/api/sensorsOnDevice?id='+DEVICE_ID+'&uuid='+UUID+'&api_key='+API_KEY+'&lang=ru').json()
+
+Ответ от сервера:
+{"sensors":[{"id":1,"type":0,"value":0,"time":1234567890,"changed":1234567890,"trend":0},{..}]}
+
+- login логин пользователя для авторизации, если он не указан, то возвращается текущий логин для uuid;
+- password - пароль авторизации на сайте
+- apikey - ключ разработчика, полученный на сайте http://narodmon.ru/
+- device id - ID устр-ва из балуна на карте(без первых букв);
--- get_temperature.py
+++ get_temperature.py
@@ -12,15 +12,7 @@ API_KEY = #apikey
 DEVICE_ID = #device id
 url_api = 'http://narodmon.ru/'
 
-#logon
 r = requests.post(url_api, data={"cmd":"userLogon","login":MYNAME,"hash":MD5HASH,"uuid":UUID,"api_key":API_KEY,"lang":"ru"})
-
-#Indicators of the device
-#resp = requests.get('http://narodmon.ru/api/sensorsOnDevice?id='+DEVICE_ID+'&uuid='+UUID+'&api_key='+API_KEY+'&lang=ru').json()
-#temp_all = 'Total temperature: ' + str(resp["sensors"][0]["value"])+resp["sensors"][0]["unit"]
-
-#Ответ сервера:
-#{"sensors":[{"id":1,"type":0,"value":0,"time":1234567890,"changed":1234567890,"trend":0},{..}]}
 
 resp = requests.get('http://narodmon.ru/api/sensorsValues?sensors='+DEVICE_ID+'&uuid='+UUID+'&api_key='+API_KEY).json()
 temp_all = 'Total temperature: ' + str(resp["sensors"][0]["value"])
